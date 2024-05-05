eabd26ed557cce99927ca6c12e6d7dc06896c24a
emaduddin678 <emaduddin678@gmail.com>
2024-05-05 12:10:57 +0600

    remove keep/react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
-import keepPreset from "keep-react/preset";
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/keep-react/**/*.{js,jsx,ts,tsx}",
-  ],
-  presets: [keepPreset],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  presets: [],
 };
