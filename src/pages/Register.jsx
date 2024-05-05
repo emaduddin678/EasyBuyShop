e521c3324bf3fa1caafa30a4e85ebb50e4da2663
emaduddin678 <emaduddin678@gmail.com>
2024-05-05 08:44:51 +0600

    router dom setup done

---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Register page</h1>
+      <p>This is Register page</p>
+    </div>
+  );
+};
+
+export default Register;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,19 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Cart from "../pages/Cart";
+import Error from "../pages/Error";
 
 const Index = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
