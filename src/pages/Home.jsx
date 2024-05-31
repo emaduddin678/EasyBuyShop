86c95f88a06c6d2b09acebbb91334b6375f1a5f5
emaduddin678 <emaduddin678@gmail.com>
2024-05-31 06:43:28 +0600

    redux checked

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import PageTitle from "../components/PageTitle";
 import ProductSidebar from "../components/ProductSidebar";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByAmount } from "../app/features/counter/counterSlice";
 
 const Home = () => {
-  const count = useSelector(state=>state.counterR.value)
+  const count = useSelector((state) => state.counterR.value);
   // console.log(count)
+  const dispatch = useDispatch();
+  const handleInc = () => {
+    dispatch(increment());
+  };
   return (
     <>
       <PageTitle title="Home" message="Home page component" />
@@ -15,6 +20,23 @@ const Home = () => {
         </div>
         <div className="main-container flex-[1]">
           <p>List of All Product {count}</p>
+          <h1>Redux checked</h1>
+          {/* <div>
+            <button className="p-2 bg-gray-500 rounded-lg" onClick={handleInc}>
+              Increment
+            </button>
+            <br />
+            <button
+              className="p-2 bg-gray-500 rounded-lg"
+              onClick={() => {
+                dispatch(decrement());
+              }}
+            >
+              decrement
+            </button>
+            <br /><br />
+            <button onClick={() => {dispatch(incrementByAmount(5));}}>Increment</button>
+          </div> */}
         </div>
       </div>
     </>
